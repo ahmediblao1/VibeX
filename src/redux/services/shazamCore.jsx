4b1184591e478a233c382b447719bca1829d4b19
ahmed iblao <ahmediblao53@gmail.com>
2023-12-24 21:13:30 +0300

    first commit

---
+++ src/redux/services/shazamCore.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable comma-dangle */
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '0004962257msh6969b611d7ca3b8p17a300jsnd0c0c5296dcc');
+
+      return headers;
+    },
+
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+
+import playerReducer from './features/playerSlice';
+import { shazamCoreApi } from './services/shazamCore';
+
+export const store = configureStore({
+  reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
+    player: playerReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamCoreApi.middleware),
+});
