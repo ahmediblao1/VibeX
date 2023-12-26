84b3a3799fb5f62c324af141bc29f80bd2408af3
ahmed iblao <ahmediblao53@gmail.com>
2023-12-26 23:15:27 +0300

    top-charts

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -35,8 +35,8 @@ const Sidebar = () => {
 
       <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
-          <RiCloseLine className="w-6 h-6 text-white mr-2 " onClick={() => setmobileMenuOpen(false)} />
-        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2 " onClick={() => setmobileMenuOpen(true)} />}
+          <RiCloseLine className="w-6 h-6 text-white mr-2 cursor-pointer " onClick={() => setmobileMenuOpen(false)} />
+        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2 cursor-pointe " onClick={() => setmobileMenuOpen(true)} />}
       </div>
 
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl
--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,79 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
+/* eslint-disable no-undef */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
+import { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Swiper } from 'swiper/react';
+import { FreeMode } from 'swiper';
+
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+const TopChartCard = ({ song, i }) => (
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e]
+   py-2 p-4 rounded-lg cursor-pointer mb-2 "
+  >
+    {song.title}
+  </div>
 );
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  const topPlays = data?.slice(0, 5);
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Chars</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">see more</p>
+          </Link>
+        </div>
+
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard
+              key={song.key}
+              song={song}
+              i={i}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+          <Link to="/top-Artists">
+            <p className="text-gray-300 text-base cursor-pointer">see more</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
