a6b91600685fd4b37c312bbfb5bad171c8f75518
ahmed iblao <ahmediblao53@gmail.com>
2023-12-29 01:17:08 +0300

    song_details

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -8,6 +8,12 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
 
       <div className="absolute inset-0 flex gap-5 items-center">
+        <img
+          alt="art"
+          src={artistId ? artist.artwork?.url.replace('w', '400').replace('h', '400')
+            : songData?.images?.coverart}
+          className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
+        />
 
         <p className=" font-bold sm:text-3xl text-xl text-white">
           {artistId ? artist.name : songData?.title }
