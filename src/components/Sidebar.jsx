feb38db763efabe10f3d05398927b3c44d049199
ahmed iblao <ahmediblao53@gmail.com>
2023-12-31 01:20:38 +0300

    bugs

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ const NavLinks = ({ handleClick }) => (
         key={item.name}
         to={item.to}
         className="flex flex-row justify-start items-center my-8
-       text-sm font-medium text-gray-400 hover:text-cyan-400"
+       text-sm font-medium text-gray-400 hover:text-cyan-100"
         onClick={() => handleClick && handleClick()}
       >
         <item.icon className="w-6 h-6 mr-2" />
