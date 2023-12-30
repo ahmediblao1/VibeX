f2b8d50e3909b5b08a199d07a3d94763bc82fc77
ahmed iblao <ahmediblao53@gmail.com>
2023-12-30 20:53:44 +0300

    top artists

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,11 +1,19 @@
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const ArtistCard = ({track}) => {
+const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
- return (
-  <div className='flex flex-col '>ArtistCard</div>;
+  return (
+    <div
+      className="flex flex-col w-[240px] p-4 bg-white/5 bg-opacity-80
+   backdrop-blur-sm animate-slideup rounded-lg cursor-pointer "
+      onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
+    >
+      <img src={track?.images?.coverart} alt={track.subtitle} className=" w-full h-56 rounded-lg" />
+      <h3 className="text-lg font-bold text-white mt-4">{track.subtitle}</h3>
 
- ) 
+    </div>
+
+  );
 };
 
 export default ArtistCard;
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import { useNavigate }  from "react-router-dom";
+import { FiSearch } from "react-icons/fi";
+
 const Searchbar = () => (
-  <div>Loader</div>
+  <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+    <label htmlFor="search-feild" className="sr-only">
+      search all songs
+    </label>
+    <div className="flex flex-row justify-start items-center">
+      <FiSearch className="w-5 h-5 ml-4" />
+      <input
+        type="search"
+        autoComplete="off"
+        name="search-feild"
+        id="search-feild"
+        placeholder="Search all songs"
+        value=""
+        onChange={() => {}}
+        className="w-full bg-transparent focus:outline-none ml-2 text-base text-white placeholder-gray-400"
+      />
+    </div>
+  </form>
 );
 
 export default Searchbar;
