360de2f54a378dce6b1f44802df6309906fe0c47
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-01-27 00:26:07 +0300

    Merge remote changes

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -23,7 +23,7 @@ const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handle
       <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
       <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.key}`}><p className="text-xl font-bold text-white">{song?.title}</p></Link>
-        <Link to={`/artists/${song?.artists[0].adamid}`}>
+        <Link to={`/artists/${song?.artists?.adamid}`}>
           <p className="text-base font-bold text-gray-300 mt-1">{song?.subtitle}</p>
         </Link>
       </div>
@@ -107,9 +107,9 @@ const TopPlay = () => {
               style={{ width: '25%', height: 'auto' }}
               className="shadow-lg rounded-full animate-slideright"
             >
-              <Link to={`/artists/${song?.artists[0]?.adamid}`}>
+              <Link to={`/artists/${song?.artists?.adamid}`}>
                 <img
-                  src={song?.images.background}
+                  src={song?.images?.background}
                   alt="name"
                   className="rounded-full w-full object-cover"
                 />
